f7ded9c76e3564b4b8c84c77502806d2db2995ca
Boril Minkov <b.minkov@student.tue.nl>
2022-03-23 18:23:54 +0200

    fml

--- app/src/main/java/com/example/messagingapp/Listing_Activity.java
+++ app/src/main/java/com/example/messagingapp/Listing_Activity.java
@@ -1,18 +1,32 @@
 package com.example.messagingapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class Listing_Activity extends AppCompatActivity {
+
+public class Listing_Activity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_messages);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
         setContentView(R.layout.activity_main);
         listing_list listing_list = new listing_list();
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -20,4 +34,22 @@ public class Listing_Activity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frame_layout, listing_list);
         fragmentTransaction.commit();
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.home:
+                return true;
+            case R.id.profileNavBar:
+                startActivity(new Intent(this,ProfileActivity.class));
+                return true;
+            case R.id.messages:
+                startActivity(new Intent(this, MessagesActivity.class));
+                return true;
+
+        }
+        return false;
+    }
+
 }
